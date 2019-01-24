92b1657e49e9e0ee2dfe9fe37999c3bbca754477
rung <suezawa@gmail.com>
2019-01-24 17:01:36 +0900

    add tokenizer

--- src/9cc.go
+++ src/9cc.go
@@ -3,7 +3,75 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
+
+type TokenType string
+
+type Token struct {
+	Ty    TokenType // tokentype
+	Val   int       // value if it's integer
+	Input string    // token string(for an error message)
+}
+
+const (
+	TK_NUM   = "INT"
+	TK_EOF   = "EOF"
+	TK_PLUS  = "+"
+	TK_MINUS = "-"
+)
+
+// トークナイズした結果のトークン列はこのスライスに保存する
+var tokens []Token
+
+func tokenize(l string) {
+	tokens = []Token{}
+	i := 0
+	// Tokenize Loop
+	// rp is ReadPointer
+	for rp := 0; rp < len(l); {
+		c := l[rp]
+
+		if isSpace(l[rp]) {
+			rp++
+			continue
+		}
+
+		if c == '+' || c == '-' {
+			t := Token{
+				Ty:    TokenType(c),
+				Input: l,
+			}
+			tokens = append(tokens, t)
+			i++
+			rp++
+			continue
+		}
+
+		if isDigit(c) {
+			num, n := readInt(l[rp:])
+			t := Token{
+				Ty:    TK_NUM,
+				Input: l,
+				Val:   num,
+			}
+			tokens = append(tokens, t)
+			i++
+			rp += n
+			continue
+		}
+
+		fmt.Fprintf(os.Stderr, "Can't tokenize '%s'\n", l[rp:])
+		os.Exit(1)
+	}
+
+	tokens = append(tokens, Token{Ty: TK_EOF})
+}
+
+func error(i int) {
+	fmt.Fprintf(os.Stderr, "Unexpected token error: %s\n", tokens[i].Input)
+	os.Exit(1)
+}
 
 func main() {
 	if len(os.Args) != 2 {
@@ -12,59 +80,65 @@ func main() {
 	}
 
 	l := os.Args[1]
+	tokenize(l)
 
 	fmt.Println(".intel_syntax noprefix")
 	fmt.Println(".global main")
 	fmt.Println("main:")
 
-	// Lexering Loop
-	// rp is ReadPointer
-	for rp := 0; rp < len(l); rp++ {
-		c := l[rp]
-
-		if rp == 0 {
-			s, n := readInt(l[rp:])
-			rp += (n-1)
-			fmt.Printf("  mov rax, %s\n", s)
-			continue
-		}
-
-		if c == '+' {
-			rp++
-			s, n := readInt(l[rp:])
-			rp += (n-1)
-			fmt.Printf("  add rax, %s\n", s)
-			continue
-		}
-
-		if c == '-' {
-			rp++
-			s, n := readInt(l[rp:])
-			rp += (n-1)
-			fmt.Printf("  sub rax, %s\n", s)
-			continue
-		}
-
-		fmt.Fprintf(os.Stderr, "Unexpected character error: '%c'\n", c)
-		os.Exit(1)
+	if tokens[0].Ty != TK_NUM {
+		error(0)
 	}
 
-	fmt.Println("  ret")
+	fmt.Printf("  mov rax, %d\n", tokens[0].Val)
+
+	i := 1
+	for tokens[i].Ty != TK_EOF {
+		if tokens[i].Ty == TK_PLUS {
+			i++
+			if tokens[i].Ty != TK_NUM {
+				error(i)
+			}
+			fmt.Printf("  add rax, %d\n", tokens[i].Val)
+			i++
+			continue
+		}
+
+		if tokens[i].Ty == "-" {
+			i++
+			if tokens[i].Ty != TK_NUM {
+				error(i)
+			}
+			fmt.Printf("  sub rax, %d\n", tokens[i].Val)
+			i++
+			continue
+		}
+		error(i)
+	}
+	fmt.Printf("  ret\n")
 	return
 }
 
 // return converted int, len
-func readInt(s string) (string, int) {
+func readInt(s string) (int, int) {
 	p := 0
-	for isDigit(s[p]){
+	for isDigit(s[p]) {
 		p++
-		if p >= len(s){
+		if p >= len(s) {
 			break
 		}
 	}
-	return s[0:p], len(s[0:p])
+	i, err := strconv.Atoi(s[0:p])
+	if err != nil {
+		panic(err)
+	}
+	return i, len(s[0:p])
 }
 
 func isDigit(ch byte) bool {
 	return '0' <= ch && ch <= '9'
+}
+
+func isSpace(ch byte) bool {
+	return ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r'
 }

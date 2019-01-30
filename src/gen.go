a7e71c656ee3417e3304d7fa59866db6480f431f
rung <suezawa@gmail.com>
2019-01-30 16:23:49 +0900

    add a variable generator

--- src/gen.go
+++ src/gen.go
@@ -1,10 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func gen(n *Node) {
 	if n.Ty == TK_NUM {
 		fmt.Printf("  push %d\n", n.Val)
+		return
+	}
+
+	if n.Ty == TK_IDENT {
+		genLval(n)
+		fmt.Println("  pop rax")
+		fmt.Println("  mov rax, [rax]")
+		fmt.Println("  push rax")
+		return
+	}
+
+	if n.Ty == "=" {
+		genLval(n.Lhs)
+		gen(n.Rhs)
+
+		fmt.Println("  pop rdi")
+		fmt.Println("  pop rax")
+		fmt.Println("  mov [rax], rdi")
+		fmt.Println("  push rdi")
 		return
 	}
 
@@ -28,4 +50,16 @@ func gen(n *Node) {
 
 	fmt.Println("  push rax")
 
+}
+
+func genLval(node *Node) {
+	if node.Ty != TK_IDENT {
+		fmt.Fprintln(os.Stderr, "代入の左辺値が変数ではありません")
+	}
+
+	// stack - charをしてアドレスをpush
+	offset := ('z' - byte(node.Name[0]) + 1) * 8
+	fmt.Println("  mov rax, rbp")
+	fmt.Printf("  sub rax, %d\n", offset)
+	fmt.Println("  push rax")
 }

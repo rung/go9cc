2e29d7382b9656d73e9f90d6b0fca20c590eed53
rung <suezawa@gmail.com>
2019-01-30 12:32:52 +0900

    add gen() function

--- src/9cc.go
+++ src/9cc.go
@@ -234,3 +234,31 @@ func term() *Node {
 
 	return nil
 }
+
+func gen(n *Node) {
+	if n.Ty == TK_NUM {
+		fmt.Printf("  push %d\n", n.Val)
+		return
+	}
+
+	gen(n.Lhs)
+	gen(n.Rhs)
+
+	fmt.Println("  pop rdi")
+	fmt.Println("  pop rax")
+
+	switch n.Ty {
+	case "+":
+		fmt.Println("  add rax, rdi")
+	case "-":
+		fmt.Println("  sub rax, rdi")
+	case "*":
+		fmt.Println("  mul rdi")
+	case "/":
+		fmt.Println("  mov rdx, 0")
+		fmt.Println("  div rdi")
+	}
+
+	fmt.Println("  push rax")
+
+}
